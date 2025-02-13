43e2892acd7e1b97070a97b74344d8d556f90c73
Zeyad_Wael <122612069+ZeyadWaell@users.noreply.github.com>
2025-02-13 15:28:39 +0200

    Adding some main Points

---
+++ ChatApp.Core/Entities/ChatMessage.cs
@@ -0,0 +1,30 @@
+﻿using ChatApp.Core.Entities.Main;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Core.Entities
+{
+    class ChatMessage : BaseEntity
+    {
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public int ChatRoomId { get; set; }
+        [ForeignKey(nameof(ChatRoomId))]
+        public ChatRoom ChatRoom { get; set; }
+
+        public bool IsBotMessage { get; set; }
+
+        public ChatBotInteraction ChatBotInteraction { get; set; }
+
+    }
+}
---
+++ ChatApp/AppMetaData/RouteConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace ChatApp.Api.AppMetaData
+{
+    public class RouteConstants
+    {
+    }
+}
